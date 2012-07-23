e74d2060967f7cb02eb88db42c36eb782fe1a090
Aad Versteden <madnificent@gmail.com>
2012-07-23 16:09:32 +0200

    allow onPropagate to return a recipe, instead of a finished instance.
    moving onPropagate upward in the execution sequence of feature.manageFeatures.  this allows us to specify a recipe in onPropagate where it previously was only possible to return i fully working enyo instance.  this should make the definition of some features somewhat simpler.
    
    Note: also added minor documentation to enyo.Component.create and enyo.Component.prototype.create because the naming is confusing.

--- enyo-hooks.js
+++ enyo-hooks.js
@@ -8,6 +8,18 @@ feature.storage = {
 
 
 feature.manageFeatures = function(inProps){
+    // propagate our prototype's features, if it has features which are set to propagate
+    // TODO: i think enyo.getObject doesn't automatically guess the default namespace
+    var superKind = inProps.kind ? ( typeof inProps.kind == "string" ? enyo.getObject( inProps.kind ) : inProps.kind ) : enyo.Control;
+    enyo.map((superKind && superKind.prototype.features) || [] ,
+             function(feature) {
+                 var newFeature = ( feature.onPropagate
+                                    && feature.onPropagate( inProps ) );
+                 if( newFeature ) {
+                     inProps.features.push( newFeature );
+                 }
+             });
+
     // instantiate any features in inProps which aren't instances yet
     inProps.features = enyo.map(inProps.features, function(feature){
         if( typeof feature == "string" ){
@@ -22,18 +34,6 @@ feature.manageFeatures = function(inProps){
         return feature;
     });
     
-    // propagate our prototype's features, if it has features which are set to propagate
-    // TODO: i think enyo.getObject doesn't automatically guess the default namespace
-    var superKind = inProps.kind ? ( typeof inProps.kind == "string" ? enyo.getObject( inProps.kind ) : inProps.kind ) : enyo.Control;
-    enyo.map((superKind && superKind.prototype.features) || [] ,
-             function(feature) {
-                 var newFeature = ( feature.onPropagate
-                                    && feature.onPropagate( inProps ) );
-                 if( newFeature ) {
-                     inProps.features.push( newFeature );
-                 }
-             });
-
     // call the atKind for each feature, thereby allowing it to modify the inProps representation
     enyo.map( inProps.features , function(feature){
         feature.atKind && feature.atKind(inProps);
@@ -52,6 +52,7 @@ for( var property in feature.storage.enyoKind ) {
     enyo.kind[property] = feature.storage.enyoKind[property];
 }
 
+// enyo.Component.create is called when createComponent is called with a recipe.  it is the equivalent of enyo.kind, but it creates an instance-like prototype (whereas enyo.kind creates a class-like prototype).
 enyo.Component.create = function(props) {
     // this.adjustComponentProps(props);
     feature.manageFeatures(props);
@@ -60,6 +61,7 @@ enyo.Component.create = function(props) {
     return feature.storage.enyoComponentCreate.apply( this , argsArray );
 }
 
+// enyo.Component.prototype.create is called when the create method of a kind is called (this is what is called when the user calls this.inherited(arguments) inside the create method of his kind.
 enyo.Component.prototype.create = function() {
     var argsArray = Array.prototype.slice.call(arguments);
     feature.storage.componentCreate.apply( this , argsArray );
